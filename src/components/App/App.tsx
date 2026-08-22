80367818ef2520b1029379a461838308de33bce7
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2026-08-22 23:56:22 +0200

    README.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,59 +1,21 @@
-import { useState } from "react";
-import "./App.module.css";
+import { useQuery } from "@tanstack/react-query";
+import noteService from "../../services/noteService";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["notes"],
+    queryFn: () =>
+      noteService.fetchNotes({
+        page: 1,
+        perPage: 12,
+      }),
+  });
 
-  return (
-    <>
-      <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  console.log(data);
+  console.log(isLoading);
+  console.log(isError);
 
-      <section id="next-steps">
-        <div id="docs">
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>Discord</li>
-            <li>X.com</li>
-            <li>Bluesky</li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  );
-}
+  return <div>NoteHub</div>;
+};
 
 export default App;
